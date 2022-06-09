4e34e7f632707587e3785979b8c7f16ae8340e00
Tanimun Hasan <59294968+tanimunhasan@users.noreply.github.com>
2022-06-09 12:43:27 +0600

    MCU_Serial_Monitor_Read

---
+++ serial.py
@@ -0,0 +1,30 @@
+import serial.tools.list_ports
+SAVE_TO_FILE=True
+
+file_name= 'output.csv'
+fid= open(file_name,'ab')
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portList = []
+
+for onePort in ports:
+    portList.append(str(onePort))
+    print(str(onePort))
+
+val = input("select Port: COM")
+
+for x in range(0, len(portList)):
+    if portList[x].startswith("COM"+ str(val)):
+        portVar = "COM" + str(val)
+        print(portList[x])
+
+serialInst.baudrate = 9600;
+serialInst.port = portVar
+serialInst.open()
+
+while True:
+    if serialInst.in_waiting:
+        packet = serialInst.readline()
+        print(packet.decode('utf'))
